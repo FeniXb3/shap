42940a8991b4312dd964ec1e02e9298c60786faa
FeniXb3 <fenix.b3@gmail.com>
2020-03-30 17:42:50 +0200

    Display cards' info a bit nicer

--- public/app.js
+++ public/app.js
@@ -4,7 +4,12 @@ document.addEventListener("DOMContentLoaded", event => {
     const db = firebase.firestore();
     const cardsRef = db.collection('cards');
 
-    getCards(cardsRef, 'Catan');
+    getCards(cardsRef, 'Catan', (cards) => {
+        const parent = document.querySelector('.row');
+        cards.forEach(doc => {
+            renderCard(parent, doc.data());
+        });
+    });
 });
 
 const auth = () => {
@@ -46,14 +51,11 @@ const addText = (text) => {
     document.body.innerHTML += html;
 };
 
-const getCards = (cardsRef, gameName) => {
+const getCards = (cardsRef, gameName, callback) => {
     const query = cardsRef.where('game', '==', gameName);
     query.get()
         .then(cards => {
-            cards.forEach(doc => {
-                const {type, subType, description, name} = doc.data();
-                addText(`${type} (${subType}) - ${name} [${description}]`);
-            });
+            callback(cards);
         });
 };
 
@@ -66,3 +68,24 @@ const addCards = (cardsRef, cards) => {
     });
 };
 
+const renderCard = (parent, data) => {
+    const {type, subType, description, name} = data;
+    const element = document.createElement('fragment');
+    element.innerHTML = `
+        <div class="col s2">
+            <div class="card orange darken-3">
+            <div class="card-content white-text">
+            
+                <span class="small">${type}</span>
+                <span class="card-title">${name ? name : subType}</span>
+                <p>${description ? description : ''}</p>
+            </div>
+            <div class="card-action white">
+                <a href="#">Play</a>
+            </div>
+            </div>
+        </div>
+      `;
+
+      parent.appendChild(element);
+};
