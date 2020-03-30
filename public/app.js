15c9918a1a295a02dfd8f799a2114299e74a4fd1
FeniXb3 <fenix.b3@gmail.com>
2020-03-30 02:51:30 +0200

    Add anonymoous login

--- public/app.js
+++ public/app.js
@@ -1,4 +1,41 @@
 document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app();
-    console.log(app);
+
+    auth();
 });
+
+const auth = () => {
+    const loading = document.getElementById("loading");
+    const button = document.getElementById("login");
+    const userNameField = document.getElementById("username");
+    firebase.auth().onAuthStateChanged(user => {
+        loading.classList.add("hidden");
+        if (user) {
+            const text = `Hello again ${user.displayName}`;
+            addText(text);
+        } else {
+            console.log(button.classList);
+            button.classList.remove("hidden");
+            userNameField.classList.remove("hidden");
+            addText('Please login');
+            button.addEventListener("click", event => {
+                const userName = userNameField.value;
+                firebase.auth().signInAnonymously()
+                    .then(result => {
+                        const user = result.user;
+                        user.updateProfile({
+                            displayName: userName
+                        }).then(() => {
+                            document.write(`Hello ${user.displayName}`);
+                        });
+                    });
+            });
+        }
+    });
+};
+
+const addText = (text) => {
+    const e = document.createElement('div');
+    e.innerText = text;
+    document.body.appendChild(e);
+};
